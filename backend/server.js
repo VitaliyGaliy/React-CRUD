9379382831ed9f5158e70406704aee24a8eb48ce
VitaliyGaliy <vitaliygaliy@gmail.com>
2017-05-15 12:57:18 +0300

    The end of the project

---
+++ backend/server.js
@@ -0,0 +1,85 @@
+import express from 'express'
+import mongodb from 'mongodb'
+import bodyParser from 'body-parser'
+
+const app = express();
+app.use(bodyParser.json());
+const dbUrl = 'mongodb://localhost/crudwithredux';
+
+function validate(data) {
+  let errors = {};
+  if (data.cover === '') errors.cover = "Can't be empty";
+  if (data.title === '') errors.title = "Can't be empty";
+  const isValid = Object.keys(errors).length === 0
+  return { errors, isValid }
+}
+
+mongodb.MongoClient.connect(dbUrl, (err, db) => {
+
+  app.get('/api/games', (req, res) => {
+    db.collection('games').find({}).toArray((err, games) => {
+      res.json({ games })
+    });
+  });
+
+  app.post('/api/games', (req, res) => {
+    const { errors, isValid } = validate(req.body)
+    if (isValid) {
+      const { title, cover } = req.body;
+      db.collection('games').insert({ title, cover }, (err, result) => {
+        if (err) {
+          res.status(500).json({ errors: { global: "Something went wrong"}});
+        } else {
+          res.json({ games: result.ops[0] })
+        }
+      })
+    } else {
+      res.status(400).json({ errors })
+    }
+  });
+
+  app.put('/api/games/:_id', (req, res) => {
+    const { errors, isValid } = validate(req.body);
+
+    if (isValid) {
+      const { title, cover } = req.body;
+      console.log('req.params', req.params)
+      db.collection('games').findOneAndUpdate(
+        { _id: new mongodb.ObjectId(req.params._id) },
+        { $set: { title, cover } },
+        { returnOriginal: false },
+        (err, result) => {
+          if (err) { res.status(500).json({ errors: { global: err }}); return; }
+
+          res.json({ game: result.value });
+        }
+      );
+    } else {
+      res.status(400).json({ errors });
+    }
+  });
+
+  app.get('/api/games/:_id', (req, res) => {
+    db.collection('games').findOne({ _id: new mongodb.ObjectId(req.params._id )}, (err, game) => {
+      res.json({ game })
+    })
+  })
+
+  app.delete('/api/games/:_id', (req, res) => {
+    db.collection('games').deleteOne({ _id: new mongodb.ObjectId(req.params._id)}, (err, r) => {
+      if (err) {re.status(500).json({ errors: { global: err }}); return}
+
+      res.json({});
+    })
+  })
+
+  app.use((req, res) => {
+    res.status(404).json({
+      errors: {
+        global: "Some errors has happened"
+      }
+    })
+  })
+
+  app.listen(8080, () =>console.log('Server is running') )
+})
--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { NavLink, Route } from 'react-router-dom'
+import GamesPage from './GamesPage'
+import GamesFormPage from './GamesFormPage'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="ui container">
+        <div className="ui three item menu">
+          <NavLink className="item" activeClassName="active" exact to="/">Home</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/games">Games</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/games/new">Add New Game</NavLink>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path="/games" component={GamesPage} />
+        <Route path="/games/new" component={GamesFormPage} />
+        <Route path="/game/:_id" component={GamesFormPage} />
       </div>
     );
   }
